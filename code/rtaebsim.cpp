6b969ae0c280b42930e95452f4f563af161b42ae
Andrea Zoli <zoli@iasfbo.inaf.it>
2015-01-23 11:20:39 +0100

    Use zmq.hqq (lot of code cleaning).

--- code/rtaebsim.cpp
+++ code/rtaebsim.cpp
@@ -2,6 +2,7 @@
  rtaebsim.cpp
  -------------------
  copyright            : (C) 2014 Andrea Bulgarelli
+                            2015 Andrea Zoli
  email                : bulgarelli@iasfbo.inaf.it
  ***************************************************************************/
 
@@ -13,149 +14,57 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <zmq.h>
+#include "zmq.hpp"
 #include <zmq_utils.h>
-
-#include <cstdlib>
-#include <cstdio>
-#include <unistd.h>
 #include <iostream>
 #include <iomanip>
-
-#include <ctime>
-#include <ctautils/mac_clock_gettime.h>
-
 #include <packet/PacketBufferV.h>
 
-using namespace PacketLib;
-
 using namespace std;
-
-struct timespec start, stop;
-unsigned long totbytes;
-unsigned long sumtotbytes;
-unsigned long npacketssent;
-unsigned long npacketssent_thread;
-unsigned long sumnpacketssent;
-
-void end(int ntimefilesize=1) {
-	clock_gettime( CLOCK_MONOTONIC, &stop);
-	double time = timediff(start, stop);
-	//std::cout << "Read " << ncycread << " ByteSeq: MB/s " << (float) (ncycread * Demo::ByteSeqSize / time) / 1048576 << std::endl;
-	cout << "Result: it took  " << time << " s" << endl;
-	cout << "Result: rate: " << setprecision(10) << totbytes / 1000000 / time << " MB/s" << endl;
-	cout << totbytes << endl;
-	//exit(1);
-}
-
-double rate() {
-	clock_gettime( CLOCK_MONOTONIC, &stop);
-	double time = timediff(start, stop);
-	//std::cout << "Read " << ncycread << " ByteSeq: MB/s " << (float) (ncycread * Demo::ByteSeqSize / time) / 1048576 << std::endl;
-	//cout << "Result: it took  " << time << " s" << endl;
-	return totbytes / (1024*1024) / time;
-}
-
-double ratepacketssent() {
-	clock_gettime( CLOCK_MONOTONIC, &stop);
-	double time = timediff(start, stop);
-	//std::cout << "Read " << ncycread << " ByteSeq: MB/s " << (float) (ncycread * Demo::ByteSeqSize / time) / 1048576 << std::endl;
-	//cout << "Result: it took  " << time << " s" << endl;
-	return npacketssent / time;
-}
-
-void freeBuff (void *data, void *hint)
-{
-	return;
-}
-
+using namespace PacketLib;
 
 int main (int argc, char *argv [])
 {
-	const char *connect_to;
-	//int message_count;
-	//int message_size;
-	void *context;
-	void *sender;
-	int rc;
-	//int i;
-	zmq_msg_t msg;
-	void *watch;
-	unsigned long elapsed;
-	
 	cout << "Start EB Sim" << endl;
+
 	// check arguments
 	if(argc < 3)
 	{
 		std::cerr << "Error: wrong number of arguments. Usage:" << std::endl;
-		std::cerr << argv[0] << " conf.xml file.raw [delay (usec)]" << std::endl;
+		std::cerr << argv[0] << " conf.xml file.raw [delay (usec)] [num messages]" << std::endl;
 		return EXIT_FAILURE;
 	}
 	
 	double usecs = 0;
-	if(argc == 3)
+	unsigned long nummessages = 300000;
+	if(argc > 3)
 		usecs = std::atof(argv[3]);
+	if(argc > 4)
+		nummessages = std::atof(argv[4]);
 	
+	zmq::context_t context;
+	zmq::socket_t sock(context, ZMQ_PUSH);
+	sock.connect("tcp://localhost:5555");
+	cout << "Connected to rtareceiver." << endl;
+
+	// load the raw file
+	PacketLib::PacketBufferV buff(argv[1], argv[2]);
+	buff.load();
+	std::cout << "Loaded buffer of " << buff.size() << " packets." << std::endl;
+
+	unsigned long message_count = 0;
+	unsigned long print_counter = 0;
+	unsigned long totbytes = 0;
+	unsigned long sumtotbytes = 0;
+
+	cout << "Sending.." << endl;
+
+	sock.send(&nummessages, sizeof(nummessages), 0);
 	
-	connect_to = "tcp://localhost:5555";
-	//connect_to = "ipc:///tmp/feeds/0";
-	//message_size = 128000;
-	//message_count = 0;
-	
-	context = zmq_init (1);
-	if (!context) {
-		printf ("error in zmq_init: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	sender = zmq_socket (context, ZMQ_PUSH);
-	if (!sender) {
-		printf ("error in zmq_socket: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	//  Add your socket options here.
-	//  For example ZMQ_RATE, ZMQ_RECOVERY_IVL and ZMQ_MCAST_LOOP for PGM.
-	rc = zmq_connect (sender, connect_to);
-	if (rc != 0) {
-		printf ("error in zmq_connect: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	//  Socket for control input
-	//void *controller = zmq_socket (context, ZMQ_SUB);
-	//zmq_connect (controller, "tcp://*:5500");
-	//zmq_setsockopt (controller, ZMQ_SUBSCRIBE, "", 0);
-	
-	cout << "start" << endl;
-	// load the raw file.
-	PacketLib::PacketBufferV* buff = NULL;
-	try
-	{
-		buff = new PacketLib::PacketBufferV(argv[1], argv[2]);
-		buff->load();
-		std::cout << "Loaded buffer of " << buff->size() << " packets." << std::endl;
-		
-	} catch(PacketException* e) {
-		cout << e->geterror() << endl;
-	}
-	
-	npacketssent = 0;
-	npacketssent_thread = 0;
-	totbytes = 0;
-	sumtotbytes = 0;
-	sumnpacketssent = 0;
-	
-	clock_gettime( CLOCK_MONOTONIC, &start);
-	size_t byteSent = 0;
-	//size_t buffsize = 128000;
-	
-	watch = zmq_stopwatch_start ();
-	while (true) {
-		
-		
+	void* watch = zmq_stopwatch_start();
+	while (message_count < nummessages) {
 		// get a Packet
-		PacketLib::ByteStreamPtr buffPtr = buff->getNext();
+		PacketLib::ByteStreamPtr buffPtr = buff.getNext();
 		
 		// wait a little
 		usleep(usecs);
@@ -165,91 +74,30 @@ int main (int argc, char *argv [])
 		
 		//copy message
 		byte* stream = buffPtr->getStream();
-	
+
 		
-		rc = zmq_msg_init_data (&msg, (void*)stream, buffsize, freeBuff, NULL);
-		if (rc != 0) {
-			printf ("error in zmq_msg_init_data: %s\n", zmq_strerror (errno));
-			return -1;
-		}
+		sock.send(stream, buffsize, 0);
 		
-		/*
-		rc = zmq_msg_init_size (&msg, buffsize);
-		if (rc != 0) {
-			printf ("error in zmq_msg_init_size: %s\n", zmq_strerror (errno));
-			return -1;
-		}
-		*/
-		
+		message_count++;
+		print_counter++;
 		totbytes += buffsize;
 		sumtotbytes += buffsize;
 		
-		//cout << "send message " << buffsize << " bytes" << endl;
-		rc = zmq_sendmsg (sender, &msg, 0);
-		//cout << "message sent" << endl;
-		if (rc < 0) {
-			printf ("error in zmq_sendmsg: %s\n", zmq_strerror (errno));
-			return -1;
-		}
-		rc = zmq_msg_close (&msg);
-		if (rc != 0) {
-			printf ("error in zmq_msg_close: %s\n", zmq_strerror (errno));
-			return -1;
-		}
-		
-		/*
-		//controller
-		zmq_pollitem_t items [] = {
-			{ controller, 0, ZMQ_POLLIN, 0 }
-		};
-		zmq_poll (items, 1, 0);
-		if (items [0].revents & ZMQ_POLLIN) {
-			
-			cout << "control message received" << endl;
-		}
-		sleep(1);
-		*/
-		
-		npacketssent++;
-		
-		sumnpacketssent++;
-		
-		// byte sent used only to send the rate to the Monitor
-		
-		//mutex.lock();
-		byteSent += buffsize;
-		npacketssent_thread++;
-		//mutex.unlock();
-		
-		if(npacketssent == 100000) {
-			elapsed = zmq_stopwatch_stop (watch);
+		if(print_counter == 100000) {
+			unsigned long elapsed = zmq_stopwatch_stop(watch);
 			if (elapsed == 0)
 				elapsed = 1;
 			
 			cout << setprecision(10) << totbytes / (1024.*1024.) / (double) elapsed * 1000000 << " MiB/s" << endl;
-			cout << setprecision(10) << npacketssent / (double) elapsed * 1000000 << " packets/s" << endl;
+			cout << setprecision(10) << print_counter / (double) elapsed * 1000000 << " packets/s" << endl;
 			cout << setprecision(10) << sumtotbytes / (1024*1024*1024) << " GiB " << endl;
-			cout << setprecision(10) << sumnpacketssent << " packets" << endl;
+			cout << setprecision(10) << message_count << " packets" << endl;
 			
 			totbytes = 0;
-			npacketssent = 0;
-			clock_gettime( CLOCK_MONOTONIC, &start);
-			watch = zmq_stopwatch_start ();
+			print_counter = 0;
+			watch = zmq_stopwatch_start();
 		}
+	}
 
-	}
-	
-	rc = zmq_close (sender);
-	if (rc != 0) {
-		printf ("error in zmq_close: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	rc = zmq_term (context);
-	if (rc != 0) {
-		printf ("error in zmq_term: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
 	return 0;
 }
--- code/rtareceiver.cpp
+++ code/rtareceiver.cpp
@@ -13,36 +13,18 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <zmq.h>
+#include "zmq.hpp"
 #include <zmq_utils.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <iostream>
+#include <iomanip>
 #include <packet/PacketBufferV.h>
 #include <packet/PacketLibDefinition.h>
-
-//#define DEBUG 1
 
 using namespace std;
 using namespace PacketLib;
 
 int main (int argc, char *argv [])
 {
-	const char *bind_to;
-	int message_count;
-	size_t message_size;
-	void *context;
-	void *s;
-	int rc;
-	//int i;
-	zmq_msg_t msg;
-	void *watch;
-	unsigned long elapsed;
-	unsigned long throughput;
-	double megabits;
-	unsigned long int totbytes = 0;
-
 	if(argc < 2)
 	{
 		std::cerr << "Error: wrong number of arguments. Usage:" << std::endl;
@@ -50,153 +32,70 @@ int main (int argc, char *argv [])
 		return EXIT_FAILURE;
 	}
 	
-	PacketStream* ps = NULL;
-	try {
-		ps = new PacketStream(argv[1]);
-		Packet *p = ps->getPacketType("triggered_telescope1_30GEN");
-		p->getPacketSourceDataField()->getFieldIndex("Number of pixels");
-		p->getPacketSourceDataField()->getFieldIndex("Number of samples");
-		p->getPacketDataFieldHeader()->getFieldIndex("TelescopeID");
-	} catch (PacketException* e)
-	{
-		cout << "Error during extractWavePacket: ";
-		cout << e->geterror() << endl;
-	}
-	
+	PacketStream ps(argv[1]);
 
-	
-	bind_to = "tcp://*:5555";
-	std::cout << "bind to " << bind_to << std::endl;
-	//bind_to = "ipc:///tmp/feeds/0";
-	
-	message_count = 0;
-	
-	context = zmq_init (1);
-	if (!context) {
-		printf ("error in zmq_init: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	s = zmq_socket (context, ZMQ_PULL);
-	if (!s) {
-		printf ("error in zmq_socket: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	//  Add your socket options here.
-	//  For example ZMQ_RATE, ZMQ_RECOVERY_IVL and ZMQ_MCAST_LOOP for PGM.
-	
-	rc = zmq_bind (s, bind_to);
-	if (rc != 0) {
-		printf ("error in zmq_bind: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	rc = zmq_msg_init (&msg);
-	if (rc != 0) {
-		printf ("error in zmq_msg_init: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	
-	rc = zmq_recvmsg (s, &msg, 0);
-	if (rc < 0) {
-		printf ("error in zmq_recvmsg: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	cout << "first message received" << endl;
-	
-	watch = zmq_stopwatch_start ();
-	
-	while(true) {
-		//cout << "receive message" << endl;
-		rc = zmq_recvmsg (s, &msg, 0);
+	zmq::context_t context;
+	zmq::socket_t sock(context, ZMQ_PULL);
+	sock.bind("tcp://*:5555");
+	cout << "Waiting the ebsim connection.." << endl;
+
+	unsigned long message_count = 0;
+	unsigned long print_counter = 0;
+	unsigned long totbytes = 0;
+	unsigned long sumtotbytes = 0;
+
+	unsigned long nummessages;
+	sock.recv(&nummessages, sizeof(nummessages), 0);
+
+	cout << "Receiving " << nummessages << " messages.." << endl;
+	void* watch = zmq_stopwatch_start ();
+
+	zmq::message_t message;
+	while(message_count < nummessages) {
+		sock.recv(&message);
 		
-		if (rc < 0) {
-			printf ("error in zmq_recvmsg: %s\n", zmq_strerror (errno));
-			return -1;
-		}
-		
-		message_count++;
-		message_size = zmq_msg_size (&msg);
-		
-		byte* stream = (byte*) zmq_msg_data (&msg);
-		
-		try {
-			ByteStreamPtr rawPacket = ByteStreamPtr(new ByteStream(stream, message_size, ps->isBigEndian()));
+		size_t buffsize = message.size();
+
+		/// decoding packetlib packet
+		PacketLib::ByteStreamPtr stream = PacketLib::ByteStreamPtr(new PacketLib::ByteStream((PacketLib::byte*)message.data(), message.size(), false));
+		PacketLib::Packet *packet = ps.getPacket(stream);
 			
-			
-			Packet *p = ps->getPacket(rawPacket);
-			
-			if(p->getPacketID() > 0) {
-				//int npix = p->getPacketSourceDataField()->getFieldValue(npix_idx);
-				//int nsamp = p->getPacketSourceDataField()->getFieldValue(nsamp_idx);
-				//int telID = p->getPacketDataFieldHeader()->getFieldValue(telid_idx);
+		if(packet->getPacketID() > 0) {
+			//int npix = p->getPacketSourceDataField()->getFieldValue(npix_idx);
+			//int nsamp = p->getPacketSourceDataField()->getFieldValue(nsamp_idx);
+			//int telID = p->getPacketDataFieldHeader()->getFieldValue(telid_idx);
 				
-				ByteStreamPtr data = p->getData();
-		#ifdef ARCH_BIGENDIAN
-				if(!data->isBigendian())
-					data->swapWord();
-		#else
-				if(data->isBigendian())
-					data->swapWord();
-		#endif
-				totbytes += data->size();
-				//byte* rawdata = data->getStream();
-			}
-	#ifdef DEBUG
-			std::cout << "npixels " << npix << std::endl;
-			std::cout << "nsamples " << nsamp << std::endl;
-			std::cout << "data size " << data->size() << std::endl;
-			std::cout << "totbytes " << totbytes << std::endl;
-	#endif
-		 
-			
+			ByteStreamPtr data = packet->getData();
+#ifdef ARCH_BIGENDIAN
+			if(!data->isBigendian())
+				data->swapWord();
+#else
+			if(data->isBigendian())
+				data->swapWord();
+#endif
+			totbytes += data->size();
+			//byte* rawdata = data->getStream();
 		}
-		catch (PacketException* e)
-		{
-				cout << "Error during extractWavePacket: ";
-				cout << e->geterror() << endl;
-		}
-		
-		//cout << "message received " << message_size << endl;
-		if(message_count == 100000) {
-			elapsed = zmq_stopwatch_stop (watch);
+
+		message_count++;
+		print_counter++;
+		totbytes += buffsize;
+		sumtotbytes += buffsize;
+
+		if(print_counter == 100000) {
+			unsigned long elapsed = zmq_stopwatch_stop(watch);
 			if (elapsed == 0)
 				elapsed = 1;
 			
-			throughput = (unsigned long)
-			((double) message_count / (double) elapsed * 1000000);
-			megabits = (double) (throughput * message_size * 8) / 1000000;
-			
-			printf ("message size: %d [B]\n", (int) message_size);
-			printf ("message count: %d\n", (int) message_count);
-			printf ("mean throughput: %d [msg/s]\n", (int) throughput);
-			printf ("mean throughput: %.3f [Mb/s]\n", (double) megabits);
-			watch = zmq_stopwatch_start ();
-			message_count = 0;
+			cout << setprecision(10) << totbytes / (1024.*1024.) / (double) elapsed * 1000000 << " MiB/s" << endl;
+			cout << setprecision(10) << print_counter / (double) elapsed * 1000000 << " packets/s" << endl;
+			cout << setprecision(10) << sumtotbytes / (1024*1024*1024) << " GiB " << endl;
+			cout << setprecision(10) << message_count << " packets" << endl;
+
+			totbytes = 0;
+			print_counter = 0;
+			watch = zmq_stopwatch_start();
 		}
-	}
-	
-	
-	
-	rc = zmq_msg_close (&msg);
-	if (rc != 0) {
-		printf ("error in zmq_msg_close: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	
-	rc = zmq_close (s);
-	if (rc != 0) {
-		printf ("error in zmq_close: %s\n", zmq_strerror (errno));
-		return -1;
-	}
-	
-	rc = zmq_term (context);
-	if (rc != 0) {
-		printf ("error in zmq_term: %s\n", zmq_strerror (errno));
-		return -1;
 	}
 	
 	return 0;
